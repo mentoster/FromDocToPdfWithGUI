eca7c47c28e4e27d1b32466d97e1b405975aed31
Дмитрий Владимирович Макаров <50594730+mentoster@users.noreply.github.com>
2020-06-02 20:44:52 +0300

    upload

---
+++ converter.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+from docx2pdf import convert
+
+sg.theme('Dark Blue 3')
+layout = ([sg.Text('Конвертировать 1 файл')],
+          [sg.Input(), sg.FileBrowse()],
+          [sg.Text('Конвертировать папку')],
+          [sg.Input(), sg.FolderBrowse()],
+          [sg.Text('Куда конвертировать папку (необязательно)')],
+          [sg.Input(), sg.FolderBrowse()],
+          [sg.Button('Начать')])
+
+window = sg.Window('docx 2 pdf', layout)
+vent, values = window.read()
+directorFile = values[0]
+directoryFolderInput = values[1]
+directoryFolderOutput = values[2]
+if directorFile != "":
+    convert(directorFile)
+if directoryFolderInput != "" and directoryFolderOutput != "":
+    convert(directoryFolderInput, directoryFolderOutput)
+if directorFile != "":
+    convert(directoryFolderInput)
+input("Нажмите Enter для выхода")
+window.close()
